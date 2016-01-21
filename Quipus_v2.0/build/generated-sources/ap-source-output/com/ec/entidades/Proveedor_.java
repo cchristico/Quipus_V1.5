d43f90cfd92af9145001a35732d01d937a05570f
DarwinGato1411 <darwinvinicio14_11@hotmail.com>
2016-01-20 22:47:46 -0500

    actualizacion

---
+++ Quipus_v2.0/build/generated-sources/ap-source-output/com/ec/entidades/Proveedor_.java
@@ -0,0 +1,17 @@
+package com.ec.entidades;
+
+import com.ec.entidades.Factura;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-20T21:47:51")
+@StaticMetamodel(Proveedor.class)
+public class Proveedor_ { 
+
+    public static volatile SingularAttribute<Proveedor, String> nombreProveedor;
+    public static volatile SingularAttribute<Proveedor, Factura> idFactu;
+    public static volatile SingularAttribute<Proveedor, Integer> idProveedor;
+    public static volatile SingularAttribute<Proveedor, String> rucProovedor;
+
+}
